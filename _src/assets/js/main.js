b62113c31db416fc1ac986a332589269f47f76e9
lauraferrandof <lauraferrandof@gmail.com>
2018-12-17 16:57:18 +0100

    Fix result painting

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -14,16 +14,16 @@ function fetchData() {
     .then(response => response.json())
     .then(data => {
       console.log(data);
-      //let resultsContent = '';
+      let resultsContent = '';
       for (let i = 0; i < data.length; i++) {
         const showName = data[i].show.name;
         if (data[i].show.image === null) {
-          // const showImage = data[i].show.image.medium;
-          resultsListEl.innerHTML += `<li><img src="https://via.placeholder.com/210x295/cccccc/666666/?text=TV" alt=""><h2>${showName}</h2></li>`;
+          resultsContent += `<li><img src="https://via.placeholder.com/210x295/cccccc/666666/?text=TV" alt=""><h2>${showName}</h2></li>`;
         } else {
-          resultsListEl.innerHTML += `<li><img src="${data[i].show.image.medium}" alt=""><h2>${showName}</h2></li>`;
+          resultsContent += `<li><img src="${data[i].show.image.medium}" alt=""><h2>${showName}</h2></li>`;
         }
       }
+      resultsListEl.innerHTML = resultsContent;
     });
 }
 
