d9df7f00622ef2996feaefaed805272ade229986
lauraferrandof <lauraferrandof@gmail.com>
2018-12-18 11:45:02 +0100

    Create logic to store show's ids in localStorage

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -36,17 +36,18 @@ function fetchData() {
       for (let i = 0; i < savedData.length; i++) {
         // Store the show's name in a constant
         const showName = savedData[i].show.name;
-        const showID = savedData[i].show.id;
 
-        //showsIDs.push(savedData[i].show.id);
+        // Store the show's id in a constant
+        const showID = savedData[i].show.id;
 
         // If there's no image available
         if (savedData[i].show.image === null) {
-          resultsContent += `<li class="results__item results__item${[i+1]}" id="${showID}"><img src="https://via.placeholder.com/210x295/cccccc/666666/?text=TV" alt=""><h2>${showName}</h2></li>`;
+          resultsContent += `<li class="results__item results__item${[i + 1]}" id="${showID}"><img src="https://via.placeholder.com/210x295/cccccc/666666/?text=TV" alt=""><h2>${showName}</h2></li>`;
         } else { // If there's an image available
-          resultsContent += `<li class="results__item results__item${[i+1]}" id="${showID}"><img src="${savedData[i].show.image.medium}" alt=""><h2>${showName}</h2></li>`;
+          resultsContent += `<li class="results__item results__item${[i + 1]}" id="${showID}"><img src="${savedData[i].show.image.medium}" alt=""><h2>${showName}</h2></li>`;
         }
       }
+      // Paint results in html
       resultsListEl.innerHTML = resultsContent;
 
       // After the list of the shows matching the user's search appears, the user can click on each show to add it to favorites
@@ -67,48 +68,22 @@ function collectShowItems() {
 function handleFavoriteShow(event) {
   // We store the clicked show in a constant
   const currentShow = event.currentTarget;
-  const currentShowID = parseInt(currentShow.getAttribute('id'));
 
   // Background-color changes and a border is added
   currentShow.classList.toggle('results__item--favorite');
 
+  // Create an array with favorite shows
   const favoriteShows = resultsListEl.querySelectorAll('.results__item--favorite');
+  console.log(favoriteShows);
 
+  // Store the favorite show's ids in another array
+  showsIDs = [];
   for (const show of favoriteShows) {
     const currentShowID = parseInt(show.getAttribute('id'));
-    if (!favoriteShows.includes(currentShow)) {
-      showsIDs.push(currentShowID);
-    }
-
+    showsIDs.push(currentShowID);
   }
-  console.log(currentShowID);
   console.log(showsIDs);
+  
+  // Store the id's array in localStorage
   localStorage.setItem('showsIDs', JSON.stringify(showsIDs));
-
-
-
-  /* for (let i = 0; i < showsIDs.length; i++) {
-    if (!JSON.parse(localStorage.getItem(showsIDs[i]))) {
-      localStorage.setItem('showsIDs', JSON.stringify(showsIDs[i]));
-    } else if (!!JSON.parse(localStorage.getItem(showsIDs[i]))) {
-      localStorage.removeItem(showsIDs[i]);
-    }
-  } */
-  //localStorage.setItem('showsIDs', JSON.stringify(showsIDs));
-
-  /* let showID = '';
-  let selectedShow = '';
-  for (let i = 0; i < showsIDs.length; i++) {
-    selectedShow = document.querySelector(`.results__item${[i+1]}`);
-    if (selectedShow.classList.contains('results__item--favorite')) {
-      showID = showsIDs[i];
-      localStorage.setItem(showID, showID);
-    }
-  } */
-
-  /* if (!selectedShow.classList.contains('results__item--favorite')) {
-    localStorage.removeItem(showID);
-  } */
-
-
 }
